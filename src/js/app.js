9f8e7c0561f5d7d450ce4432eac0a5c90377bc44
RubyComp <alex61225@gmail.com>
2023-07-14 18:54:00 +0300

    hero adaptive

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
 	console.log('Scripts');
+
+	const HEADER = 'header'
+	const COMPACT = HEADER + '_compact'
+
+	const header = document.querySelector('.' + HEADER)
+
+	const checkScrollPosition = () => {
+		if (
+			window.hasOwnProperty('scrollY')
+			&& window.scrollY > 90
+		)
+			header.classList.add(COMPACT)
+		else
+			header.classList.remove(COMPACT)
+	}
+
+	checkScrollPosition()
+	window.addEventListener('scroll', checkScrollPosition)
+
 })
 
