6177c49fd6bdc2c339e4ad1738926a9db10e9076
RubyComp <alex61225@gmail.com>
2023-07-28 17:23:10 +0300

    pages

--- src/js/app.js
+++ src/js/app.js
@@ -23,88 +23,93 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	/* */
 
-	const carousel = document.querySelector('.carousel'),
-	firstImg = document.querySelectorAll('.carousel section')[0];
+	const carousel = document.querySelector('.carousel');
 
-	// const arrowIcons = document.querySelectorAll('.carousel-wrapper i');
+	if (carousel) {
 
-	let isDragStart = false,
-		isDragging = false,
-		prevPageX,
-		prevScrollLeft,
-		positionDiff;
+		const firstImg = document.querySelectorAll('.carousel section')[0]
 
-	// arrowIcons.forEach((icon) => {
-	// 	icon.addEventListener('click', () => {
-	// 		let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
-	// 		// if clicked icon is left, reduce width value from the carousel scroll left else add to it
-	// 		carousel.scrollLeft += icon.id == 'left' ? -firstImgWidth : firstImgWidth;
-	// 	});
-	// });
+		// const arrowIcons = document.querySelectorAll('.carousel-wrapper i');
 
-	const autoSlide = () => {
-		// if there is no image left to scroll then return from here
-		if (carousel.scrollLeft == carousel.scrollWidth - carousel.clientWidth)
-			return;
+		let isDragStart = false,
+			isDragging = false,
+			prevPageX,
+			prevScrollLeft,
+			positionDiff;
 
-		positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
-		let firstImgWidth = firstImg.clientWidth + 14;
-		// getting difference value that needs to add or reduce from carousel left to take middle img center
-		let valDifference = firstImgWidth - positionDiff;
+		// arrowIcons.forEach((icon) => {
+		// 	icon.addEventListener('click', () => {
+		// 		let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+		// 		// if clicked icon is left, reduce width value from the carousel scroll left else add to it
+		// 		carousel.scrollLeft += icon.id == 'left' ? -firstImgWidth : firstImgWidth;
+		// 	});
+		// });
 
-		if (carousel.scrollLeft > prevScrollLeft) {
-			// return console.log('user is scrolling to the right');
-			// if user is scrolling to the right
-			return (carousel.scrollLeft +=
-				positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff);
-		}
-		//   console.log('user is scrolling to the left');
-		// if user is scrolling to the left
-		carousel.scrollLeft -=
-			positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-	};
+		const autoSlide = () => {
+			// if there is no image left to scroll then return from here
+			if (carousel.scrollLeft == carousel.scrollWidth - carousel.clientWidth)
+				return;
 
-	const dragStart = (e) => {
-		// updating global variables value on mouse down event
-		isDragStart = true;
-		prevPageX = e.pageX || e.touches[0].pageX;
-		prevScrollLeft = carousel.scrollLeft;
-	};
+			positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
+			let firstImgWidth = firstImg.clientWidth + 14;
+			// getting difference value that needs to add or reduce from carousel left to take middle img center
+			let valDifference = firstImgWidth - positionDiff;
 
-	const dragging = (e) => {
-		// scrolling images/carousel to left according to mouse pointer
-		if (!isDragStart) return;
-		e.preventDefault();
-		isDragging = true;
-		carousel.classList.add('dragging');
-		positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-		carousel.scrollLeft = prevScrollLeft - positionDiff;
-	};
+			if (carousel.scrollLeft > prevScrollLeft) {
+				// return console.log('user is scrolling to the right');
+				// if user is scrolling to the right
+				return (carousel.scrollLeft +=
+					positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff);
+			}
+			//   console.log('user is scrolling to the left');
+			// if user is scrolling to the left
+			carousel.scrollLeft -=
+				positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+		};
 
-	const dragStop = () => {
-		isDragStart = false;
-		carousel.classList.remove('dragging');
+		const dragStart = (e) => {
+			// updating global variables value on mouse down event
+			isDragStart = true;
+			prevPageX = e.pageX || e.touches[0].pageX;
+			prevScrollLeft = carousel.scrollLeft;
+		};
 
-		if (!isDragging) return;
-			isDragging = false;
-			autoSlide();
-	};
+		const dragging = (e) => {
+			// scrolling images/carousel to left according to mouse pointer
+			if (!isDragStart) return;
+			e.preventDefault();
+			isDragging = true;
+			carousel.classList.add('dragging');
+			positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+			carousel.scrollLeft = prevScrollLeft - positionDiff;
+		};
 
-	carousel.addEventListener('mousedown', dragStart);
-	carousel.addEventListener('touchstart', dragStart);
+		const dragStop = () => {
+			isDragStart = false;
+			carousel.classList.remove('dragging');
 
-	carousel.addEventListener('mousemove', dragging);
-	carousel.addEventListener('touchmove', dragging);
+			if (!isDragging) return;
+				isDragging = false;
+				autoSlide();
+		};
 
-	carousel.addEventListener('mouseup', dragStop);
-	carousel.addEventListener('mouseleave', dragStop);
-	carousel.addEventListener('touchend', dragStop);
+		carousel.addEventListener('mousedown', dragStart);
+		carousel.addEventListener('touchstart', dragStart);
 
+		carousel.addEventListener('mousemove', dragging);
+		carousel.addEventListener('touchmove', dragging);
+
+		carousel.addEventListener('mouseup', dragStop);
+		carousel.addEventListener('mouseleave', dragStop);
+		carousel.addEventListener('touchend', dragStop);
+
+	}
 
 	/**/
 	const sections = document.querySelectorAll('.faq__section');
 
-		// Разворачиваем первую секцию при загрузке страницы
+	if (sections) {
+
 		if (sections.length > 0) {
 			sections[0].classList.add('active');
 			const firstAnswer = sections[0].querySelector('.faq__answer');
@@ -136,8 +141,9 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	
 		sections.forEach((section) => {
-		const question = section.querySelector('.faq__question');
-		question.addEventListener('click', toggleAccordion);
-	});
+			const question = section.querySelector('.faq__question');
+			question.addEventListener('click', toggleAccordion);
+		});
+	}
 
 })
