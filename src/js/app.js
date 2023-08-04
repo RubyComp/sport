39792fdcb098448fc523ebc99084bca36aa803a2
RubyComp <alex61225@gmail.com>
2023-08-04 08:54:12 +0300

    favicon, links, forms

--- src/js/app.js
+++ src/js/app.js
@@ -146,4 +146,40 @@ document.addEventListener('DOMContentLoaded', function () {
 		});
 	}
 
+	
+	////////////////////////////////
+	// Timer
+	const countdownContainer = document.getElementById("countdown-container");
+
+	const targetDate = new Date("2023-12-31T23:59:59").getTime();
+
+	function startCountdown() {
+		const currentDate = new Date().getTime();
+
+		const timeRemaining = targetDate - currentDate;
+
+		const daysElement = document.getElementById("days");
+		const hoursElement = document.getElementById("hours");
+		const minutesElement = document.getElementById("minutes");
+		const secondsElement = document.getElementById("seconds");
+
+		let days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+		let hours = Math.floor(
+			(timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+		);
+		let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+		let seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+		hours = String(hours).padStart(2, "0");
+		minutes = String(minutes).padStart(2, "0");
+		seconds = String(seconds).padStart(2, "0");
+
+		daysElement.textContent = days;
+		hoursElement.textContent = hours;
+		minutesElement.textContent = minutes;
+		secondsElement.textContent = seconds;
+
+	}
+
+	setInterval(startCountdown, 1000);
+
 })
